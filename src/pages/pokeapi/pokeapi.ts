857eca7cc1a28e46b2cf09e727af6b1cabbca55b
leonel.santiago <leonel.santiago@TICTC-LP26W10>
2018-06-17 03:05:55 -0400

    - Add of card to divide the properties of pokemons - Add of loading method and alert method to enhance the ux.

--- src/pages/pokeapi/pokeapi.ts
+++ src/pages/pokeapi/pokeapi.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PokeapiServiceProvider} from '../../providers/pokeapi-service/pokeapi-service'
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+
 
 /**
  * Generated class for the PokeapiPage page.
@@ -16,23 +19,46 @@ import { PokeapiServiceProvider} from '../../providers/pokeapi-service/pokeapi-s
 })
 export class PokeapiPage {
   pokemonName: string;
-  pokemon: any[] = []
+  pokemon: any[] = [];
+  pokemonNotFound: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public pokeapiService: PokeapiServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public pokeapiService: PokeapiServiceProvider, public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
   }
 
   getPokemonByName(pokemonName){
     this.pokeapiService.getPokemonByName(this.pokemonName)
     .subscribe(
       (data) => {
-        this.pokemon.push(data);
+          this.presentLoading();
+          this.pokemon.push(data);
       },(error) => {
         console.error(error);
+        this.showAlert();
       }
     )
     this.pokemon = [];
+    this.pokemonNotFound = "";
   }
   
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: `Buscando el Pokemon: ${this.pokemonName}...`,
+      duration: 3000
+    });
+    loader.present();
+  }
+
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Pokemon no encontrado!',
+      subTitle: 'Favor verificar que el nombre del pokemon buscado es el correcto!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
   ionViewDidLoad() {
     // console.log('ionViewDidLoad PokeapiPage');
